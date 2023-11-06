41d532110f108bf9d2cb2aa39285ee5e073245f1
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-11-06 20:46:31 +0100

    Customize footer

---
+++ frontend/src/addons/volto-sneridagh-dev/src/customizations/@kitconcept/volto-light-theme/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+// SemanticUI-free pre-@plone/components
+import { UniversalLink, Logo } from '@plone/volto/components';
+import Container from '@kitconcept/volto-light-theme/components/Atoms/Container/Container';
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <Container className="footer">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="footer-message">Resistance is Futile</div>
+        <a className="item powered-by" href="https://plone.org">
+          Powered by Plone
+        </a>
+        <div className="footer-branding">
+          Made with{' '}
+          <span role="img" aria-label="love" style={{ color: 'red' }}>
+            ❤️
+          </span>{' '}
+          by <a href="https://github.com/sneridagh">@sneridagh</a>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;

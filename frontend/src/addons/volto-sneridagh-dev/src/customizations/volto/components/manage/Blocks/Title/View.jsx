26893af23eab32e82c10fdb2e346176a5d916116
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-12-17 13:07:18 +0100

    Fix custom post title only for posts

--- frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
+++ frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
@@ -1,18 +1,26 @@
 import { FormattedDate } from '@plone/volto/components';
 
 const TitleBlockView = ({ properties, metadata }) => {
-  return (
-    <div className="documentFirstHeading">
-      <h1>{(metadata || properties)['title'] || ''}</h1>
-      <div className="byline">
-        by Víctor Fernández de Alba on{' '}
-        <FormattedDate
-          date={properties.effective || properties.modified}
-          locale="es"
-        />
+  if (properties['@type'] === 'Post') {
+    return (
+      <div className="documentFirstHeading">
+        <h1>{(metadata || properties)['title'] || ''}</h1>
+        <div className="byline">
+          by Víctor Fernández de Alba on{' '}
+          <FormattedDate
+            date={properties.effective || properties.modified}
+            locale="es"
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <h1 className="documentFirstHeading">
+        {(metadata || properties)['title'] || ''}
+      </h1>
+    );
+  }
 };
 
 export default TitleBlockView;

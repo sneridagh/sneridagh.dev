2154aa0997f0d44626255f9f507429d8d41ada4d
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-01-25 16:32:04 +0100

    Upgrade

--- frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
+++ frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
@@ -7,10 +7,7 @@ const TitleBlockView = ({ properties, metadata }) => {
         <h1>{(metadata || properties)['title'] || ''}</h1>
         <div className="byline">
           by Víctor Fernández de Alba on{' '}
-          <FormattedDate
-            date={properties.effective || properties.modified}
-            locale="es"
-          />
+          <FormattedDate date={properties.effective || properties.modified} />
         </div>
       </div>
     );

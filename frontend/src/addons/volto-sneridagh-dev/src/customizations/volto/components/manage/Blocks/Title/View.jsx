8b9365db41bcfe69150c83eb004265017cda3a40
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-01-26 14:08:36 +0100

    Upgrade to 18a9 (#11)
    * Upgrade to 18a9
    
    * locale for dates

--- frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
+++ frontend/src/addons/volto-sneridagh-dev/src/customizations/volto/components/manage/Blocks/Title/View.jsx
@@ -7,7 +7,10 @@ const TitleBlockView = ({ properties, metadata }) => {
         <h1>{(metadata || properties)['title'] || ''}</h1>
         <div className="byline">
           by Víctor Fernández de Alba on{' '}
-          <FormattedDate date={properties.effective || properties.modified} />
+          <FormattedDate
+            date={properties.effective || properties.modified}
+            locale="es"
+          />
         </div>
       </div>
     );

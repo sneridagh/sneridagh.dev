eff786ae8f73a6979f4fb2b46babe68f8262ef5c
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-06 19:57:06 +0100

    Upgrade to 18a10, leadimage support

---
+++ frontend/src/addons/volto-sneridagh-dev/src/index.ts
@@ -0,0 +1,30 @@
+import { ConfigData } from '@plone/types';
+import LogoImage from './sneridaghLogo.png';
+
+const applyConfig = (config: ConfigData) => {
+  config.settings = {
+    ...config.settings,
+    isMultilingual: false,
+    supportedLanguages: ['en'],
+    defaultLanguage: 'en',
+    matomoUrlBase: 'https://analytics.sneridagh.dev/',
+    matomoSiteId: '1',
+  };
+
+  config.settings.enableFatMenu = false;
+
+  config.registerComponent({
+    name: 'LogoImage',
+    component: LogoImage,
+  });
+
+  config.blocks.blocksConfig.leadimage.restricted = ({ properties }) =>
+    !(
+      properties.hasOwnProperty('image') ||
+      properties.hasOwnProperty('preview_image')
+    );
+
+  return config;
+};
+
+export default applyConfig;
